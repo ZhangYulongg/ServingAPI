743a4802c7e15e63fb7e1abc1161d6ff39c83c5e
ZhangYulongg <1272816783@qq.com>
2021-07-07 17:28:42 +0800

    add

--- paddle_serving_server/test_server.py
+++ paddle_serving_server/test_server.py
@@ -3,12 +3,12 @@ import pytest
 import subprocess
 import paddle_serving_server.serve
 import time
-from paddle_serving_server.server import Server
+from paddle_serving_server.server import Server, MultiLangServer
 import paddle_serving_server as serving
 import argparse
 import collections
 from multiprocessing import Process
-from paddle_serving_client import Client
+from paddle_serving_client import Client, MultiLangClient
 import numpy as np
 
 
@@ -45,7 +45,7 @@ class TestServer(object):
               -0.0332]])
         fetch_map = client.predict(
             feed={"x": data}, fetch=["price"], batch=True)
-        print(fetch_map)
+        print("fetch_map:", fetch_map)
         return fetch_map['price']
 
     def test_load_model_config(self):
@@ -81,6 +81,7 @@ class TestServer(object):
         self.test_server.check_local_bin()
         assert self.test_server.use_local_bin is True
         assert self.test_server.bin_path == os.environ["SERVING_BIN"]
+        del os.environ["SERVING_BIN"]
 
     def test_get_fetch_list(self):
         assert self.test_server.get_fetch_list() == ['price']
@@ -125,39 +126,99 @@ class TestServer(object):
         assert os.system(f"grep -r model_toolkit_file {self.dir}/workdir/resource.prototxt") == 0
         assert os.system(f"grep -r engines {self.dir}/workdir/general_infer_0/model_toolkit.prototxt") == 0
 
-    # def test_run_server_with_cpu(self):
-    #     self.test_server.prepare_server("workdir", 9696, "cpu")
-    #     p = Process(target=self.test_server.run_server)
-    #     p.start()
-    #     time.sleep(5)
-    #
-    #     price = self.predict()
-    #     assert price == np.array([[18.901152]], dtype=np.float32)
-    #
-    #     os.system("kill `ps -ef | grep serving | awk '{print $2}'` > /dev/null 2>&1")
-    #
-    # def test_run_server_with_gpu(self):
-    #     self.test_server.set_gpuid(0)
-    #     self.test_server.prepare_server("workdir_0", 9696, "gpu")
-    #     p = Process(target=self.test_server.run_server)
-    #     p.start()
-    #     time.sleep(10)
-    #
-    #     price = self.predict()
-    #     assert price == np.array([[18.901152]], dtype=np.float32)
-    #
-    #     os.system("kill `ps -ef | grep serving | awk '{print $2}'` > /dev/null 2>&1")
+    def test_run_server_with_cpu(self):
+        self.test_server.prepare_server("workdir", 9696, "cpu")
+        p = Process(target=self.test_server.run_server)
+        p.start()
+        time.sleep(5)
+
+        price = self.predict()
+        assert price == np.array([[18.901152]], dtype=np.float32)
+
+        os.system("kill `ps -ef | grep serving | awk '{print $2}'` > /dev/null 2>&1")
+
+    def test_run_server_with_gpu(self):
+        self.test_server.prepare_server("workdir_0", 9696, "gpu")
+        self.test_server.set_gpuid("0,1")
+        p = Process(target=self.test_server.run_server)
+        p.start()
+        time.sleep(10)
+
+        price = self.predict()
+        assert price == np.array([[18.901152]], dtype=np.float32)
+
+        os.system("kill `ps -ef | grep serving | awk '{print $2}'` > /dev/null 2>&1")
+
+
+class TestMultiLangServer(object):
+    def setup(self):
+        op_maker = serving.OpMaker()
+        op_seq_maker = serving.OpSeqMaker()
+        read_op = op_maker.create('general_reader')
+        op_seq_maker.add_op(read_op)
+        infer_op_name = "general_infer"
+        general_infer_op = op_maker.create(infer_op_name)
+        op_seq_maker.add_op(general_infer_op)
+        general_response_op = op_maker.create('general_response')
+        op_seq_maker.add_op(general_response_op)
+
+        dir = os.path.dirname(os.path.abspath(__file__))
+        self.dir = dir
+        self.model_dir = dir + "/uci_housing_model"
+        self.test_server = MultiLangServer()
+        self.test_server.set_op_sequence(op_seq_maker.get_op_sequence())
+        self.test_server.load_model_config(self.model_dir)
+
+    def teardown(self):
+        os.system("rm -rf workdir*")
+        os.system("rm -rf PipelineServingLogs")
+
+    def predict(self):
+        client = MultiLangClient()
+        client.connect(["127.0.0.1:9696"])
+
+        data = np.array(
+            [[0.0137, -0.1136, 0.2553, -0.0692, 0.0582, -0.0727, -0.1583, -0.0584, 0.6283, 0.4919, 0.1856, 0.0795,
+              -0.0332]])
+        fetch_map = client.predict(
+            feed={"x": data}, fetch=["price"], batch=True)
+        print("fetch_map:", fetch_map)
+        return fetch_map['price']
+
+    def test_load_model_config(self):
+        assert self.test_server.is_multi_model_ is False
+        assert self.test_server.bclient_config_path_list == [self.model_dir]
+
+    def test_prepare_server(self):
+        self.test_server.prepare_server(workdir="workdir", port=9696, device="gpu", use_encryption_model=False,
+                                        cube_conf=None)
+        assert self.test_server.device == "gpu"
+        assert self.test_server.port_list_ == [12000]
+        assert self.test_server.gport_ == 9696
+
+    def test_run_server(self):
+        self.test_server.set_gpuid("0,1")
+        self.test_server.prepare_server(workdir="workdir", port=9696, device="gpu", use_encryption_model=False,
+                                        cube_conf=None)
+        p = Process(target=self.test_server.run_server)
+        p.start()
+        time.sleep(10)
+        os.system("netstat -nlp")
+        os.system("ps -ef")
+
+        price = self.predict()
+        assert price == np.array([[18.901152]], dtype=np.float32)
+
+        os.system("kill `ps -ef | grep serving | awk '{print $2}'` > /dev/null 2>&1")
+        os.system("kill `ps -ef | grep python | awk '{print $2}'` > /dev/null 2>&1")
 
 
 if __name__ == '__main__':
-    # args = argparse.ArgumentParser().parse_args()
-    # args.thread = 10
-    # print(args.thread, type(args.thread), type(args))
     # test_load_model_config()
     # test_port_is_available_with_used_port()
     # pytest.main(["-sv", "test_server.py"])
     # TestServer().test_get_fetch_list()
-    ts = TestServer()
+    ts = TestMultiLangServer()
     ts.setup()
-    ts.test_prepare_engine()
+    ts.test_run_server()
     pass
