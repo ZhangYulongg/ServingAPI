8d29238cb52c2541609512bc17e8d96015c88d6d
ZhangYulongg <1272816783@qq.com>
2021-10-11 20:35:30 +0800

    add

--- CE/cpp_ocr/run.sh
+++ CE/cpp_ocr/run.sh
@@ -13,7 +13,7 @@ fi
 
 # 修改det feed type
 cp -r ${DATA_PATH}/ocr/ocr_det_client ${CODE_PATH}/Serving/python/examples/ocr/
-sed -i "s/feed_type: 1/feed_type: 3/g" ${CODE_PATH}/Serving/python/examples/ocr/ocr_det_client/serving_client_conf.prototxt
+sed -i "s/feed_type: 1/feed_type: 20/g" ${CODE_PATH}/Serving/python/examples/ocr/ocr_det_client/serving_client_conf.prototxt
 sed -i "s/shape: 3/shape: 1/g" ${CODE_PATH}/Serving/python/examples/ocr/ocr_det_client/serving_client_conf.prototxt
 sed -i '7,8d' ${CODE_PATH}/Serving/python/examples/ocr/ocr_det_client/serving_client_conf.prototxt
 
