29524b748c472c52a56eec53e5c8a353fe71e35a
ZhangYulongg <1272816783@qq.com>
2021-10-22 18:19:24 +0800

    add

---
+++ CI/run_38.sh
@@ -0,0 +1,44 @@
+export FLAGS_call_stack_level=2
+
+# check bin
+if [ ! -f "${CODE_PATH}/Serving/server-build-gpu-opencv/core/general-server/serving" ]; then
+    echo "compile failed!"
+    exit 1
+fi
+
+rm -rf result.txt
+cases="test_for_ci38.py"
+#cases=`find ./ -maxdepth 1 -name "test*.py" | sort`
+echo $cases
+ignore=""
+bug=0
+
+job_bt=`date '+%Y%m%d%H%M%S'`
+echo "============ failed cases =============" >> result.txt
+for file in ${cases}
+do
+    echo ${file}
+    if [[ ${ignore} =~ ${file##*/} ]]; then
+        echo "跳过"
+    else
+        if [[ ${ce_name} =~ "cpu" ]]; then
+            $py_version -m pytest --disable-warnings -sv ${file} -k "cpu"
+        else
+            $py_version -m pytest --disable-warnings -sv ${file}
+        fi
+        if [[ $? -ne 0 && $? -ne 5 ]]; then
+            echo ${file} >> result.txt
+            bug=`expr ${bug} + 1`
+        fi
+    fi
+done
+job_et=`date '+%Y%m%d%H%M%S'`
+
+echo "total bugs: "${bug} >> result.txt
+if [ ${bug} != 0 ]; then
+    cp result.txt ${output_dir}/result_${py_version}.txt
+fi
+cat result.txt
+cost=$(expr $job_et - $job_bt)
+echo "$cost s"
+exit ${bug}
