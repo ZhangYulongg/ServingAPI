0bbb6b6c2c31092a9f9950abe820e75250dc4884
ZhangYulongg <1272816783@qq.com>
2021-10-25 20:21:37 +0800

    add

--- CE/run_ce.sh
+++ CE/run_ce.sh
@@ -2,6 +2,9 @@ shell_path=${CODE_PATH}/CE
 cd ${shell_path}
 bash -x pip_install.sh $1 $2
 
+# 暂时适配cuda11.2镜像
+mv /home/TensorRT-8.0.3.4 /usr/local/
+cp -rf /usr/local/TensorRT-8.0.3.4/include/* /usr/include/ && cp -rf /usr/local/TensorRT-8.0.3.4/lib/* /usr/lib/
 unset http_proxy && unset https_proxy
 $py_version -m pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple
 $py_version download_bin.py > load_bin 2>&1
