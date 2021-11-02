b74ca0f3693a1681f34462a01169e9148948a33f
ZhangYulongg <1272816783@qq.com>
2021-11-02 17:44:59 +0800

    add

--- CE/windows/release.py
+++ CE/windows/release.py
@@ -1,8 +1,10 @@
 import os
 import psutil
+import sys
 pids = psutil.pids()
 for pid in pids:
     p = psutil.Process(pid)
     if p.name() == 'python.exe':
         cmd = 'taskkill /F /IM python.exe'
         os.system(cmd)
+sys.exit(0)
