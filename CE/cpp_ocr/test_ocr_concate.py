2747ee010d0da565fd7a91584d5328da7a99d9a2
ZhangYulongg <1272816783@qq.com>
2021-11-12 10:21:49 +0800

    update ocr async case

--- CE/cpp_ocr/test_ocr_concate.py
+++ CE/cpp_ocr/test_ocr_concate.py
@@ -243,7 +243,7 @@ class TestOCR(object):
     def test_gpu_cpp_async(self):
         # 1.start server
         self.serving_util.start_server_by_shell(
-            cmd=f"{self.serving_util.py_version} -m paddle_serving_server.serve --model ocr_det_model ocr_rec_model --op_num 2 4 --op_max_batch 16 --gpu_ids 0,1 1 --port 9293",
+            cmd=f"{self.serving_util.py_version} -m paddle_serving_server.serve --model ocr_det_model ocr_rec_model --runtime_thread_num 2 4 --batch_infer_size 16 --gpu_ids 0,1 1 --port 9293",
             sleep=17,
         )
 
