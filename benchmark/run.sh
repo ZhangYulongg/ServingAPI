571b7d6d332136afcfb6240144c5f9f3792360f7
ZhangYulongg <1272816783@qq.com>
2022-01-13 17:35:03 +0800

    CE: fix benchmark scripts

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -5,6 +5,6 @@ bash compile.sh $1 $2 $3
 unset http_proxy && unset https_proxy
 $py_version -m pip install psutil -i https://mirror.baidu.com/pypi/simple
 $py_version -m pip install openpyxl -i https://mirror.baidu.com/pypi/simple
-$py_version -m pip install pandas -i https://mirror.baidu.com/pypi/simple
+#$py_version -m pip install pandas -i https://mirror.baidu.com/pypi/simple
 tail -10 load_bin
 bash run_benchmark.sh $1 $2
