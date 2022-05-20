47ffa2c15e1dfa16584671986fe7a28fe774c71d
ZhangYulongg <1272816783@qq.com>
2022-05-20 12:15:04 +0800

    CE:update paddle docker case

--- CE/paddle_docker/run_paddle_1027.sh
+++ CE/paddle_docker/run_paddle_1027.sh
@@ -2,10 +2,15 @@
 export http_proxy=${proxy}
 export https_proxy=${proxy}
 
-wget https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda10.2-cudnn7.tar.gz --no-check-certificate
-tar -zxf TensorRT6-cuda10.2-cudnn7.tar.gz -C /usr/local
-cp -rf /usr/local/TensorRT-6.0.1.8/include/*  /usr/include/ && cp -rf /usr/local/TensorRT-6.0.1.8/lib/* /usr/lib/
-rm -rf TensorRT6-cuda10.2-cudnn7.tar.gz
+#wget https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda10.2-cudnn7.tar.gz --no-check-certificate
+#tar -zxf TensorRT6-cuda10.2-cudnn7.tar.gz -C /usr/local
+#cp -rf /usr/local/TensorRT-6.0.1.8/include/*  /usr/include/ && cp -rf /usr/local/TensorRT-6.0.1.8/lib/* /usr/lib/
+#rm -rf TensorRT6-cuda10.2-cudnn7.tar.gz
+
+wget https://paddle-ci.gz.bcebos.com/TRT/TensorRT7-cuda10.2-cudnn8.tar.gz --no-check-certificate
+tar -zxf TensorRT7-cuda10.2-cudnn8.tar.gz -C /usr/local
+cp -rf /usr/local/TensorRT-7.1.3.4/include/*  /usr/include/ && cp -rf /usr/local/TensorRT-7.1.3.4/lib/* /usr/lib/
+rm -rf TensorRT7-cuda10.2-cudnn8.tar.gz
 
 apt install -y libcurl4-openssl-dev libbz2-dev
 wget https://paddle-serving.bj.bcebos.com/others/centos_ssl.tar && tar xf centos_ssl.tar && rm -rf centos_ssl.tar && mv libcrypto.so.1.0.2k /usr/lib/libcrypto.so.1.0.2k && mv libssl.so.1.0.2k /usr/lib/libssl.so.1.0.2k && ln -sf /usr/lib/libcrypto.so.1.0.2k /usr/lib/libcrypto.so.10 && ln -sf /usr/lib/libssl.so.1.0.2k /usr/lib/libssl.so.10 && ln -sf /usr/lib/libcrypto.so.10 /usr/lib/libcrypto.so && ln -sf /usr/lib/libssl.so.10 /usr/lib/libssl.so
