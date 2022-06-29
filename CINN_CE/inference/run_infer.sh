02fd2ab24e3d20200e45543834c6613ae76852f1
ZhangYulongg <1272816783@qq.com>
2022-06-29 16:39:16 +0800

    add cinn ce

--- CINN_CE/inference/run_infer.sh
+++ CINN_CE/inference/run_infer.sh
@@ -31,13 +31,13 @@ function main() {
     sed -i "s/repeat = 10/repeat = 1000/" test_squeezenet.py
     sed -i "s/repeat = 10/repeat = 1000/" test_facedet.py
 
-    ${py_version} test_resnet18.py $PWD/ResNet18 ON | tee -a ${log_dir}/ResNet18.log 2>&1
-    ${py_version} test_mobilenetv2.py $PWD/MobileNetV2 ON | tee -a ${log_dir}/MobileNetV2.log 2>&1
-    ${py_version} test_efficientnet.py $PWD/EfficientNet ON | tee -a ${log_dir}/EfficientNet.log 2>&1
-    ${py_version} test_mobilenetv1.py $PWD/MobilenetV1 ON | tee -a ${log_dir}/MobilenetV1.log 2>&1
-    ${py_version} test_resnet50.py $PWD/ResNet50 ON | tee -a ${log_dir}/ResNet50.log 2>&1
-    ${py_version} test_squeezenet.py $PWD/SqueezeNet ON | tee -a ${log_dir}/SqueezeNet.log 2>&1
-    ${py_version} test_facedet.py $PWD/FaceDet ON | tee -a ${log_dir}/FaceDet.log 2>&1
+    ${py_version} test_resnet18.py $PWD/ResNet18 ON 2>&1 | tee -a ${log_dir}/ResNet18.log
+    ${py_version} test_mobilenetv2.py $PWD/MobileNetV2 ON 2>&1 | tee -a ${log_dir}/MobileNetV2.log
+    ${py_version} test_efficientnet.py $PWD/EfficientNet ON 2>&1 | tee -a ${log_dir}/EfficientNet.log
+    ${py_version} test_mobilenetv1.py $PWD/MobilenetV1 ON 2>&1 | tee -a ${log_dir}/MobilenetV1.log
+    ${py_version} test_resnet50.py $PWD/ResNet50 ON 2>&1 | tee -a ${log_dir}/ResNet50.log
+    ${py_version} test_squeezenet.py $PWD/SqueezeNet ON 2>&1 | tee -a ${log_dir}/SqueezeNet.log
+    ${py_version} test_facedet.py $PWD/FaceDet ON 2>&1 | tee -a ${log_dir}/FaceDet.log
 }
 
 main
