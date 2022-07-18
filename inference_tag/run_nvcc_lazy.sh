90a834910c61ff1275588889a9b5f3a02d267618
ZhangYulongg <1272816783@qq.com>
2022-07-18 20:27:18 +0800

    add

--- inference_tag/run_nvcc_lazy.sh
+++ inference_tag/run_nvcc_lazy.sh
@@ -25,7 +25,7 @@ echo ">>> Generate yaml configuration file ..."
 bash prepare_config.sh
 
 echo ">>> Run onnx_convert and converted model diff checker ..."
-bash onnx_convert.sh
+bash onnx_convert_nvcc.sh
 
 echo ">>> Run inference_benchmark ..."
 bash inference_benchmark_daily_nvcc.sh
